8735ac649e07d049ce08a081d2980eb49dbac6e7
Matt Williams <matt1256@gmail.com>
2023-03-20 15:15:24 +0900

    Update jpeg-png-to-avif-webp.sh

--- jpeg-png-to-avif-webp.sh
+++ jpeg-png-to-avif-webp.sh
@@ -1,25 +1,35 @@
-#!/bin/sh
+#!/bin/bash
 
-# Define the folder that contains the static image assets
-IMAGES_FOLDER="path/to/images"
+# Define the folder where the static assets are located
+STATIC_FOLDER="./static"
 
-# Loop through all the image files in the folder
-for file in $IMAGES_FOLDER/*.{jpg,jpeg,png}; do
-    # Get the file extension
-    ext="${file##*.}"
+# Find all image files in the static folder and its subdirectories
+IMAGES=$(find "$STATIC_FOLDER" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" \))
 
-    # Convert the image to either the .avif or .webp format
-    if [ "$ext" = "jpg" ] || [ "$ext" = "jpeg" ]; then
-        cwebp -q 75 "$file" -o "${file%.*}.webp"
-    elif [ "$ext" = "png" ]; then
-        avifenc -j 8 "$file" -o "${file%.*}.avif"
-    fi
+# Loop through each image file found
+for IMAGE in $IMAGES; do
+  # Get the filename and extension of the image
+  FILENAME=$(basename "$IMAGE")
+  EXTENSION="${FILENAME##*.}"
 
-    # Replace the original asset with the converted asset
-    if [ -f "${file%.*}.webp" ] || [ -f "${file%.*}.avif" ]; then
-        rm "$file"
-        mv "${file%.*}.webp" "$file" || mv "${file%.*}.avif" "$file"
-    fi
+  # Convert the image to .avif or .webp format using cwebp or avifenc
+  if [ "$EXTENSION" == "jpg" ] || [ "$EXTENSION" == "jpeg" ]; then
+    OUTPUT_FORMAT="webp"
+    cwebp -quiet "$IMAGE" -o "${IMAGE%.*}.$OUTPUT_FORMAT"
+  elif [ "$EXTENSION" == "png" ]; then
+    OUTPUT_FORMAT="avif"
+    avifenc -quiet "$IMAGE" "${IMAGE%.*}.$OUTPUT_FORMAT"
+  fi
+
+  # Replace the original image with the converted one
+  if [ -f "${IMAGE%.*}.$OUTPUT_FORMAT" ]; then
+    # Get the directory structure of the original image
+    DIR="$(dirname "$IMAGE")"
+    # Make sure the destination folder exists
+    mkdir -p "$DIR"
+    # Replace the original image with the converted one
+    mv -f "${IMAGE%.*}.$OUTPUT_FORMAT" "$DIR/$FILENAME"
+  fi
 done
 
 exit 0
